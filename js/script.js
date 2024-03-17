0c71f4564ce73593896a9fe6f986831208d2b190
CristiniLorenzo <lorenzo.cristini98@gmail.com>
2024-03-17 20:04:19 +0100

    grid done

--- js/script.js
+++ js/script.js
@@ -3,3 +3,32 @@
 // Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 // Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 
+// bottone cliccabile 
+const btn = document.querySelector('#play-button');
+// popolo la glriglia con i quadrati
+btn.addEventListener('click', function() {
+    const grid = document.querySelector('#grid'); 
+    for(let i = 1; i <= 100; i++) {
+        const newSquare = generateSquare(i);
+        grid.append(newSquare);
+    }
+});
+
+// FUNCTIONS 
+
+// funzione che popola la griglia 
+function generateSquare(number) {
+    const newSquare = document.createElement('div');
+    newSquare.classList.add('square');
+    newSquare.innerHTML = `<span>${number}</span>`;
+
+    // eventi al click sul quadrato 
+    newSquare.addEventListener('click', function() {
+        // cambio colore 
+        newSquare.classList.add('click');
+        // alert 
+        alert(number);
+    });
+
+    return newSquare
+}
